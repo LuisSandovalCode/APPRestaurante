76bbe0eb816d7fc7c196dccfa81e3d80996f0cd1
David Gómez Brenes <davidgxb@gmail.com>
2019-08-24 14:24:19 -0600

    Implementacion de metodos para la negacion en UtilidadUINavegacion -Aplicar Pop a master detal (Volver de pagina) -Aplicar Push a master detal (Ir a pagina)

--- Restaurante/Restaurante.APP/Restaurante.APP/ViewModel/Utilidades/UtilidadesUI/UtilidadUINavegacion.cs
+++ Restaurante/Restaurante.APP/Restaurante.APP/ViewModel/Utilidades/UtilidadesUI/UtilidadUINavegacion.cs
@@ -17,5 +17,21 @@ namespace Restaurante.APP.ViewModel.Utilidades.UtilidadesUI
                 Detail = navigation
             };
         }
+
+        /// <summary>
+        /// Aplica un push sobre el MasterDetailPage
+        /// </summary>
+        /// <param name="page">Object view redirect</param>
+        public static void IrAView(Page page)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail.Navigation.PushAsync(page);
+        }
+        /// <summary>
+        /// Aplica un pop sobre la pagina actual
+        /// </summary>
+        public static void VolverDeView()
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail.Navigation.PopAsync();//Se devuelve a la anterior
+        }
     }
 }

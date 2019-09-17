ba67b3361800ca93c2745ca8ee91019ee0e42cf4
Luis Sandoval <sandoval.luis2097@gmail.com>
2019-09-16 20:16:33 -0600

    Registrar Usuario con Imagen de Perfil

--- Restaurante/Restaurante.APP/Restaurante.APP.Android/MainActivity.cs
+++ Restaurante/Restaurante.APP/Restaurante.APP.Android/MainActivity.cs
@@ -6,12 +6,19 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
+using System.IO;
+using Android.Content;
+using Restaurante.APP.Droid.Servicios;
 
 namespace Restaurante.APP.Droid
 {
     [Activity(Label = "Restaurante.APP", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static readonly int PickImageId = 1000;
+        public static MainActivity Instance { get; private set; }
+        public TaskCompletionSource<Stream> ImagenSeleccionada { get; set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -21,6 +28,8 @@ namespace Restaurante.APP.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.DependencyService.Register<GaleriaServicio>();
+            Instance = this;
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -28,6 +37,27 @@ namespace Restaurante.APP.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent IntentoResultado)
+        {
+            base.OnActivityResult(requestCode, resultCode, IntentoResultado);
+
+            if(requestCode.Equals(PickImageId))
+            {
+                if(resultCode == Result.Ok && IntentoResultado != null)
+                {
+                    Android.Net.Uri vloUriImagen = IntentoResultado.Data;
+
+                    Stream vloStreamImagen = ContentResolver.OpenInputStream(vloUriImagen);
+
+                    ImagenSeleccionada.SetResult(vloStreamImagen);
+                }
+                else
+                {
+                    ImagenSeleccionada.SetResult(null);
+                }
+            }
         }
     }
 }
--- Restaurante/Restaurante.APP/Restaurante.APP.Android/Resources/Resource.designer.cs
+++ Restaurante/Restaurante.APP/Restaurante.APP.Android/Resources/Resource.designer.cs
@@ -5778,26 +5778,26 @@ namespace Restaurante.APP.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020135
-			public const int avd_hide_password_1 = 2130837813;
-			
 			// aapt resource value: 0x7f020136
-			public const int avd_hide_password_2 = 2130837814;
+			public const int avd_hide_password_1 = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int avd_hide_password_3 = 2130837815;
+			public const int avd_hide_password_2 = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int avd_hide_password_3 = 2130837816;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020138
-			public const int avd_show_password_1 = 2130837816;
-			
 			// aapt resource value: 0x7f020139
-			public const int avd_show_password_2 = 2130837817;
+			public const int avd_show_password_1 = 2130837817;
 			
 			// aapt resource value: 0x7f02013a
-			public const int avd_show_password_3 = 2130837818;
+			public const int avd_show_password_2 = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int avd_show_password_3 = 2130837819;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -6450,11 +6450,11 @@ namespace Restaurante.APP.Droid
 			// aapt resource value: 0x7f02012e
 			public const int notification_icon_background = 2130837806;
 			
-			// aapt resource value: 0x7f020133
-			public const int notification_template_icon_bg = 2130837811;
-			
 			// aapt resource value: 0x7f020134
-			public const int notification_template_icon_low_bg = 2130837812;
+			public const int notification_template_icon_bg = 2130837812;
+			
+			// aapt resource value: 0x7f020135
+			public const int notification_template_icon_low_bg = 2130837813;
 			
 			// aapt resource value: 0x7f02012f
 			public const int notification_tile_bg = 2130837807;
@@ -6467,6 +6467,9 @@ namespace Restaurante.APP.Droid
 			
 			// aapt resource value: 0x7f020132
 			public const int tooltip_frame_light = 2130837810;
+			
+			// aapt resource value: 0x7f020133
+			public const int user = 2130837811;
 			
 			static Drawable()
 			{
@@ -9453,7 +9456,10 @@ namespace Restaurante.APP.Droid
 		{
 			
 			// aapt resource value: 0x7f080000
-			public const int xamarin_essentials_fileprovider_file_paths = 2131230720;
+			public const int network_security_config = 2131230720;
+			
+			// aapt resource value: 0x7f080001
+			public const int xamarin_essentials_fileprovider_file_paths = 2131230721;
 			
 			static Xml()
 			{

2d6eae336be0d4b5752c363c99a6588ce5536f37
Luis Sandoval <sandoval.luis2097@gmail.com>
2019-09-17 23:47:40 -0600

    Carga de Restaurantes y Reservacion por medio de API

---
+++ Restaurante/Restaurante.APP/Restaurante.APP/ExternalServices/SVRestaurante.cs
@@ -0,0 +1,71 @@
+﻿using Restaurante.APP.ConfiguracionApp;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante.APP.ExternalServices
+{
+    public class SVRestaurante
+    {
+        private static string URLWebApi { get; set; }
+        private static string Controller { get; set; }
+
+        public SVRestaurante()
+        {
+            if (string.IsNullOrEmpty(URLWebApi))
+                URLWebApi = AppSettingsManager.Settings["Service"];
+            if (string.IsNullOrEmpty(Controller))
+                Controller = AppSettingsManager.Settings["RestauranteController"];
+        }
+
+        public async Task<string> ObtenerRestaurantes()
+        {
+            string vlcJsonRespuesta = string.Empty;
+            string URLObtenerRestaurantes = URLWebApi + Controller + nameof(ObtenerRestaurantes);
+
+            using (var HttpCliente = new HttpClient())
+            {
+                var vloHttpRespuesta = await HttpCliente.PostAsync(URLObtenerRestaurantes,
+                            new StringContent("",
+                            Encoding.UTF8, "application/json"));
+
+                if (vloHttpRespuesta.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    vlcJsonRespuesta = await vloHttpRespuesta.Content.ReadAsStringAsync();
+                }
+            }
+
+            return vlcJsonRespuesta;
+        }
+
+        public async Task<bool> RealizarReseracion(string JsonReservacion)
+        {
+            try
+            {
+                bool reservo = false;
+                string URLObtenerRestaurantes = URLWebApi + Controller + nameof(RealizarReseracion);
+
+                using (var HttpCliente = new HttpClient())
+                {
+                    var vloHttpRespuesta = await HttpCliente.PostAsync(URLObtenerRestaurantes,
+                                new StringContent(JsonReservacion,
+                                Encoding.UTF8, "application/json"));
+
+                    if (vloHttpRespuesta.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        reservo = true;
+                    }
+                }
+
+                return reservo;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Restaurante/Restaurante.APP/Restaurante.APP/Model/Restaurante/RestauranteModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Restaurante.APP.Model.Restaurante
+{
+    public class RestauranteModel
+    {
+
+        public string CedulaUsuario { get; set; }
+
+        public int Id { get; set; }
+
+        public ImageSource Imagen { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Pais { get; set; }
+    }
+}
